db7362fb197df6863b84b7d85814f3722edbd155
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2021-10-01 22:07:39 +0300

    Устранение дублирования кода в меню

---
+++ src/includes/menu-content.php
@@ -0,0 +1,53 @@
+<h5>Для кошек</h5>
+<a href="catalog.php?category=cat-feed">
+    <h6>Корм</h6>
+</a>
+<a href="catalog.php?category=cat-bowls">
+    <h6>Миски</h6>
+</a>
+<a href="catalog.php?category=cat-beds-and-houses">
+    <h6>Лежанки и домики</h6>
+</a>
+<a href="catalog.php?category=cat-care-products">
+    <h6>Средства для ухода</h6>
+</a>
+<a href="catalog.php?category=cat-ammunition">
+    <h6>Амуниция</h6>
+</a>
+<a href="catalog.php?category=cat-toilets">
+    <h6>Туалеты</h6>
+</a>
+<a href="catalog.php?category=cat-toys">
+    <h6>Игрушки</h6>
+</a>
+
+<h5 class="mt-4">Для собак</h5>
+<a href="catalog.php?category=dog-feed">
+    <h6>Корм</h6>
+</a>
+<a href="catalog.php?category=dog-bowls">
+    <h6>Миски</h6>
+</a>
+<a href="catalog.php?category=dog-beds-and-houses">
+    <h6>Лежанки и домики</h6>
+</a>
+<a href="catalog.php?category=dog-care-products">
+    <h6>Средства для ухода</h6>
+</a>
+<a href="catalog.php?category=dog-clothes-and-footwear">
+    <h6>Одежда и обувь</h6>
+</a>
+<a href="catalog.php?category=dog-ammunition">
+    <h6>Амуниция</h6>
+</a>
+<a href="catalog.php?category=dog-toys">
+    <h6>Игрушки</h6>
+</a>
+
+<h5 class="mt-4">О нас</h5>
+<a href="#">
+    <h6>Доставка и оплата</h6>
+</a>
+<a href="#">
+    <h6>Контакты</h6>
+</a>
