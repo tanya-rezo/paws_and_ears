7e787dd613d2df68b8134cb6b5732ea15a1f32d1
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2021-10-08 23:29:50 +0300

    Редирект на HTTPS и обработка 404 и 403

---
+++ src/404.php
@@ -0,0 +1,12 @@
+<?php include './includes/header.php'; ?>
+
+<div class="container container-fill">
+    <div class="vh-center flex-column-container mt-150px">
+        <img class="error-cat-img" src="img/laptop-cat.svg"></img>
+        <h3 class="mt-3">Страница не найдена</h3>
+        <!-- <span>Скоро с вами свяжется менеджер</span> -->
+        <a role="button" class="btn btn-primary  mt-3" href="index.php">На главную</a>
+    </div>
+</div>
+
+<?php include './includes/footer.php'; ?>
