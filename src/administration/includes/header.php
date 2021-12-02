6d6d47ea49fc95a3e70a018f1cd5cc1e5f543f93
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2021-12-02 03:31:07 +0300

    Админка: верстка главной страницы

---
+++ src/administration/includes/header.php
@@ -0,0 +1,16 @@
+<?php include_once '../database.php'; ?>
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="theme-color" content="#FAFAFA">
+    <link rel="shortcut icon" href="../favicon/favicon.ico" />
+
+    <title>Лапки и ушки - Администрирование</title>
+    <link href="../css/style.css" rel="stylesheet" />
+</head>
+
+<body>
---
+++ src/administration/index.php
@@ -0,0 +1,72 @@
+<?php include './includes/header.php'; ?>
+
+<div class="container main-container">
+    <div class="vh-center">
+        <img class="admin-logo" src="../img/logo.svg">
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="pet-type">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">2</h1>
+                    <h3 class="admin-tile-text mr-3">Виды животных</h3>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="categories">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">14</h1>
+                    <h3 class="admin-tile-text mr-3">Категории</h3>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="products">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">14</h1>
+                    <h3 class="admin-tile-text mr-3">Товары</h3>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="brands">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">2</h1>
+                    <h3 class="admin-tile-text mr-3">Бренды</h3>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="manufacturing-countries">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">3</h1>
+                    <h3 class="admin-tile-text mr-3 text-right">Страны<br>производители</h3>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="orders">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">8</h1>
+                    <h3 class="admin-tile-text mr-3 text-right">Оформленные<br>заказы</h3>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a class="text-decoration-none" role="button" href="clients">
+                <div class="admin-tile flex-column-container justify-content-around align-items-end">
+                    <h1 class="admin-tile-text mr-3">9</h1>
+                    <h3 class="admin-tile-text mr-3">Клиенты</h3>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+<?php include '../includes/footer.php';
