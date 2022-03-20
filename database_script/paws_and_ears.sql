894fc09741281ecdbeab147eebe393da82859f16
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2022-03-20 20:40:49 +0300

    Добавление даты заказа в БД

--- database_script/paws_and_ears.sql
+++ database_script/paws_and_ears.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 06 2021 г., 01:02
+-- Время создания: Мар 20 2022 г., 20:20
 -- Версия сервера: 5.6.41
 -- Версия PHP: 5.5.38
 
@@ -144,7 +144,8 @@ CREATE TABLE `placed_order` (
   `client_id` int(11) NOT NULL,
   `total_cost` decimal(11,0) NOT NULL,
   `total_discount` decimal(11,0) NOT NULL,
-  `total_product_count` int(11) NOT NULL
+  `total_product_count` int(11) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
