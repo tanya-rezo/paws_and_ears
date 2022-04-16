dadab86efc703198c29b51de014276ae60bb1a93
Tanya Rezo <32035077+tanya-rezo@users.noreply.github.com>
2022-04-16 23:51:06 +0300

    При очистке корзины удалять только переменные сессии представляющие собой товары из корзины

--- src/cart/clear.php
+++ src/cart/clear.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 
-// удаляем все переменные сессии
-// т.к. в сессии хранятся только товары из корзины то это равнозначно чистке корзины
-session_unset();
+// удаляем все товары из сессии
+foreach ($_SESSION as $key => $value) {
+    if (substr($key, 0, 8) == "product_") {
+        unset($_SESSION[$key]);
+    }
+}
 
 header('Location: /cart.php');
